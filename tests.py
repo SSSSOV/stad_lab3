76393cfe2854c2ffcd628869cc791eba8b0f0c79
Aleksandr <98227655+SSSSOV@users.noreply.github.com>
2025-03-28 14:30:18 +0700

    test 4: other сonversion

--- tests.py
+++ tests.py
@@ -20,6 +20,17 @@ class TestLengthConversion(unittest.TestCase):
     result = converter.convert_length(100, 'centimeters', 'meters')
     self.assertAlmostEqual(result, 1, 5, "Неточность в переводе 100 см в м!")
 
+  def test_other_сonversion(self):
+    converter = Converter()
+    
+    result = converter.convert_length(1, 'meters', 'centimeters')
+    self.assertAlmostEqual(result, 100, 5, "Неточность в переводе 1 м в см!")
+
+    result = converter.convert_length(1, 'meters', 'inches')
+    self.assertAlmostEqual(result, 39.3701, 5, "Неточность в переводе 1 м в дюймы!")
+
+    result = converter.convert_length(1, 'inches', 'centimeters')
+    self.assertAlmostEqual(result, 2.54, 5, "Неточность в переводе 1 дюйма в см!")
 
 if __name__ == '__main__':
   unittest.main()
