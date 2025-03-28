1158eb5d21b7e0ec28ae83cb3c34fd0ccdc0c94c
Aleksandr <98227655+SSSSOV@users.noreply.github.com>
2025-03-28 14:46:09 +0700

    change of principle, conversion through meters

--- converter.py
+++ converter.py
@@ -1,10 +1,12 @@
 class Converter:
+  LENGTH_COEFFS = {
+    'inches': 0.0254,
+    'centimeters': 0.01,
+    'meters': 1.0,
+  }
+
   def __init__(self):
     pass
 
   def convert_length(self, value, from_unit, to_unit):
-    if(from_unit == 'centimeters' and to_unit == 'inches'):
-      return value / 2.54
-    if(from_unit == 'centimeters' and to_unit == 'meters'):
-      return value / 100
-    else: return -1
+    return value * self.LENGTH_COEFFS[from_unit] / self.LENGTH_COEFFS[to_unit]
--- tests.py
+++ tests.py
@@ -9,28 +9,27 @@ class TestConverterCreation(unittest.TestCase):
     self.assertIsNotNone(converter)
 
 class TestLengthConversion(unittest.TestCase):
-  # 1 см = 0.393701 дюйма
   def test_centimeters_to_inches(self):
     converter = Converter()
     result = converter.convert_length(10, 'centimeters', 'inches')
-    self.assertAlmostEqual(result, 3.93701, 5, "Неточность в переводе 10 см в дюймы!")
+    self.assertAlmostEqual(result, 3.93701, 4, "Неточность в переводе 10 см в дюймы!")
   
   def test_centimeters_to_meters(self):
     converter = Converter()
     result = converter.convert_length(100, 'centimeters', 'meters')
-    self.assertAlmostEqual(result, 1, 5, "Неточность в переводе 100 см в м!")
+    self.assertAlmostEqual(result, 1, 4, "Неточность в переводе 100 см в м!")
 
   def test_other_сonversion(self):
     converter = Converter()
     
     result = converter.convert_length(1, 'meters', 'centimeters')
-    self.assertAlmostEqual(result, 100, 5, "Неточность в переводе 1 м в см!")
+    self.assertAlmostEqual(result, 100, 4, "Неточность в переводе 1 м в см!")
 
     result = converter.convert_length(1, 'meters', 'inches')
-    self.assertAlmostEqual(result, 39.3701, 5, "Неточность в переводе 1 м в дюймы!")
+    self.assertAlmostEqual(result, 39.3701, 4, "Неточность в переводе 1 м в дюймы!")
 
     result = converter.convert_length(1, 'inches', 'centimeters')
-    self.assertAlmostEqual(result, 2.54, 5, "Неточность в переводе 1 дюйма в см!")
+    self.assertAlmostEqual(result, 2.54, 4, "Неточность в переводе 1 дюйма в см!")
 
 if __name__ == '__main__':
   unittest.main()
