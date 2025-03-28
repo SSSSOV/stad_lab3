7393b1b774851c40edbff4cec3618b58f63e9b34
Aleksandr <98227655+SSSSOV@users.noreply.github.com>
2025-03-28 13:52:00 +0700

    test 2: convert centimeters to inches

--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+# 7. Конвертер различных величин (американская система, старорусская, СИ).
+
 import unittest
 from converter import Converter
 
@@ -5,6 +7,13 @@ class TestConverterCreation(unittest.TestCase):
   def test_converter_instance(self):
     converter = Converter()
     self.assertIsNotNone(converter)
+
+class TestLengthConversion(unittest.TestCase):
+  # 1 см = 0.393701 дюйма
+  def test_centimeters_to_inches(self):
+    converter = Converter()
+    result = converter.convert_length(10, 'centimeters', 'inches')
+    self.assertAlmostEqual(result, 3.93701, 5, "Неточность в переводе 10 см в дюймы")
 
 if __name__ == '__main__':
   unittest.main()
